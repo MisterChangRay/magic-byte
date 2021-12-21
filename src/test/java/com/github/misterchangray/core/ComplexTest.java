fc4988f65cf55d44d1e48abd030515360fe08cdb
misterchangray@hotmail.com <misterchangray@hotmail.com>
2021-12-21 15:40:17 +0800

    fix: fix the list fill error

--- src/test/java/com/github/misterchangray/core/ComplexTest.java
+++ src/test/java/com/github/misterchangray/core/ComplexTest.java
@@ -1,12 +1,80 @@
 package com.github.misterchangray.core;
 
 
+import com.github.misterchangray.core.entity.Classes;
 import com.github.misterchangray.core.entity.Student;
 import com.github.misterchangray.core.entity.custom.UnknownType;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ComplexTest {
+
+    /**
+     * test for array byte fill.
+     *
+     */
+    @Test
+    public void testListOverflow() throws InterruptedException {
+        Classes classes1 = Classes.build(1).get(0);
+        Classes classes2 = Classes.build(1).get(0);
+
+        classes1.getStudentList().addAll(classes2.getStudentList());
+
+        byte[] bytes = MagicByte.unpackToByte(classes1);
+        Classes pack = MagicByte.pack(bytes, Classes.class);
+
+        for (int i = 0; i < pack.getStudentList().size(); i++) {
+            Student student1 = classes1.getStudentList().get(i);
+            Student student2 = pack.getStudentList().get(i);
+            Assert.assertEquals(student1.getPhone(), student2.getPhone());
+            Assert.assertEquals(student1.getName(), student2.getName());
+            Assert.assertArrayEquals(student1.getBookids(), student2.getBookids());
+
+        }
+    }
+
+    /**
+     * test for array byte fill.
+     *
+     */
+    @Test
+    public void testListFill() throws InterruptedException {
+        Classes classes1 = Classes.build(1).get(0);
+
+        classes1.getStudentList().remove(0);
+
+        byte[] bytes = MagicByte.unpackToByte(classes1);
+        Classes pack = MagicByte.pack(bytes, Classes.class);
+
+        for (int i = 0; i < classes1.getStudentList().size(); i++) {
+            Student student1 = classes1.getStudentList().get(i);
+            Student student2 = pack.getStudentList().get(i);
+            Assert.assertEquals(student1.getPhone(), student2.getPhone());
+            Assert.assertEquals(student1.getName(), student2.getName());
+            Assert.assertArrayEquals(student1.getBookids(), student2.getBookids());
+
+        }
+
+    }
+
+
+    /**
+     * test for array byte fill.
+     *
+     */
+    @Test
+    public void testArrayOverflow() throws InterruptedException {
+        Student student = Student.build(1).get(0);
+        student.setBookids(new int[]{22, 33, 55, 66});
+
+        byte[] bytes = MagicByte.unpackToByte(student);
+        Student pack = MagicByte.pack(bytes, Student.class);
+
+
+        Assert.assertEquals(student.getPhone(), pack.getPhone());
+        Assert.assertArrayEquals(pack.getBookids(), new int[]{22, 33, 55});
+
+    }
 
     /**
      * test for array byte fill.
