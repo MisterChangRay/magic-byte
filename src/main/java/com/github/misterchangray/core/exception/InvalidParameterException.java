38b1b4c677c11fc743f26ada03870d420ec4137f
ray <misterchangray@hotmail.com>
2023-11-27 15:00:11 +0800

    fix the calcCheckCode

--- src/main/java/com/github/misterchangray/core/exception/InvalidParameterException.java
+++ src/main/java/com/github/misterchangray/core/exception/InvalidParameterException.java
@@ -2,7 +2,6 @@ package com.github.misterchangray.core.exception;
 
 public class InvalidParameterException extends MagicByteException {
 
-    public InvalidParameterException(){ }
 
     public InvalidParameterException(String str){
         super(str);
