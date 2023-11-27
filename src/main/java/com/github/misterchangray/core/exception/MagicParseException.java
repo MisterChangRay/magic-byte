b139184193ceaf99692bf49a3186091e5418987e
ray <misterchangray@hotmail.com>
2023-11-27 15:00:25 +0800

    fix the calcCheckCode

--- src/main/java/com/github/misterchangray/core/exception/MagicParseException.java
+++ src/main/java/com/github/misterchangray/core/exception/MagicParseException.java
@@ -2,7 +2,6 @@ package com.github.misterchangray.core.exception;
 
 public class MagicParseException extends MagicByteException {
 
-    public MagicParseException(){ }
 
     public MagicParseException(String str){
         super(str);
