2477d10ae924c882bf8a5e29e6db6af2a1a09c91
ray <misterchangray@hotmail.com>
2023-11-27 14:59:05 +0800

    fix the calcCheckCode

---
+++ src/test/java/com/github/misterchangray/core/autochecker/pojo/CheckCode4BytesWithArrByte.java
@@ -0,0 +1,62 @@
+package com.github.misterchangray.core.autochecker.pojo;
+
+import com.github.misterchangray.core.annotation.MagicClass;
+import com.github.misterchangray.core.annotation.MagicField;
+
+import java.util.List;
+
+
+@MagicClass
+public class CheckCode4BytesWithArrByte {
+    @MagicField(order = 1)
+    private int head;
+    @MagicField(order = 3, calcLength = true)
+    private int length;
+    @MagicField(order = 5, size = 10)
+    private String name;
+    @MagicField(order = 7, size = 10)
+    private String addr;
+    @MagicField(order = 8, size = 4, calcCheckCode = true)
+    private Byte[] checkCodes;
+
+
+    public Byte[] getCheckCodes() {
+        return checkCodes;
+    }
+
+    public void setCheckCodes(Byte[] checkCodes) {
+        this.checkCodes = checkCodes;
+    }
+
+    public int getHead() {
+        return head;
+    }
+
+    public void setHead(int head) {
+        this.head = head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
