cd11ba4f93e06815d6481a8f27c75e067347cfc4
ray <misterchangray@hotmail.com>
2023-11-27 15:00:29 +0800

    fix the calcCheckCode

--- src/main/java/com/github/misterchangray/core/exception/OutOfMemoryDetecteException.java
+++ src/main/java/com/github/misterchangray/core/exception/OutOfMemoryDetecteException.java
@@ -2,7 +2,6 @@ package com.github.misterchangray.core.exception;
 
 public class OutOfMemoryDetecteException extends MagicByteException {
 
-    public OutOfMemoryDetecteException(){ }
 
     public OutOfMemoryDetecteException(String str){
         super(str);
