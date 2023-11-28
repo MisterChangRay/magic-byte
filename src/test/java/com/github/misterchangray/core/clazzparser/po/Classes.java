55fb898e74cff4745eeda5deace8bf56afd137ac
ray <misterchangray@hotmail.com>
2023-11-28 13:06:00 +0800

    refactor dynamicSizeOf to dynamicSizeOfId

---
+++ src/test/java/com/github/misterchangray/core/clazzparser/po/Classes.java
@@ -0,0 +1,47 @@
+package com.github.misterchangray.core.clazzparser.po;
+
+import com.github.misterchangray.core.annotation.MagicField;
+
+public class Classes {
+    @MagicField(order = 1)
+    private int id;
+    @MagicField(order = 2, size = 10)
+    private String name;
+    @MagicField(order = 3, size = 1)
+    private Student[] student;
+
+    @MagicField(order = 4)
+    private Book book;
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student[] getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student[] student) {
+        this.student = student;
+    }
+}
