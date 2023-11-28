60dcc8d23b1a0af55b2fdfbc3e78fb5bd66135f6
ray <misterchangray@hotmail.com>
2023-11-28 13:07:04 +0800

    refactor dynamicSizeOf to dynamicSizeOfId

---
+++ src/test/java/com/github/misterchangray/core/dynamicsize/pojo/error/DuplicatedNestedHead2.java
@@ -0,0 +1,28 @@
+package com.github.misterchangray.core.dynamicsize.pojo.error;
+
+import com.github.misterchangray.core.annotation.MagicClass;
+import com.github.misterchangray.core.annotation.MagicField;
+
+@MagicClass
+public class DuplicatedNestedHead2 {
+    @MagicField(id = "len1", order = 1)
+    private short len;
+    @MagicField( order = 2)
+    private DuplicatedNestedHead head2;
+
+    public short getLen() {
+        return len;
+    }
+
+    public void setLen(short len) {
+        this.len = len;
+    }
+
+    public DuplicatedNestedHead getHead2() {
+        return head2;
+    }
+
+    public void setHead2(DuplicatedNestedHead head2) {
+        this.head2 = head2;
+    }
+}
